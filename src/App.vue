7fda2e2dee2b8126824d17b1197d77c401292e75
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-02 15:49:20 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  {{app_title}}
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+
+  // Components
   components: {
-    HelloWorld
+
+  },
+
+  // Data Examples
+  data() {
+    return {
+      app_title: 'TVS-VUE',
+
+      todos: [
+
+        {
+          id: 1,
+          title: "todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "todo three",
+          completed: false
+        }
+      ]
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
