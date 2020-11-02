05d1ee365b705daa264b9f2f8714714ff52a71f4
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-02 21:41:33 +0800

    v-bind for props, vfor and vbind for unique key

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>Todos</h1>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{ todo.title }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Todos",
+  props: ["todos"],
 };
 </script>
 
